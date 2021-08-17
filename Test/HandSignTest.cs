21768d3e78b8320ac4620af605051ef6113e9834
parulgupta2609 <70070691+parulgupta2609@users.noreply.github.com>
2021-08-17 08:18:30 -0500

    Add files via upload

--- Test/HandSignTest.cs
+++ Test/HandSignTest.cs
@@ -1,1 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+namespace RockPaperScissors.Test
+{
+    [TestFixture()]
+    public class HandSignTest
+    {
+        [Test()]
+        public void TestGetWinner()
+        {
+            Player player1 = new Player("John");
+            Player player2 = new Player("CPU");
 
+            player1.HandSign = new HandSign(HandSign.Move.Paper);
+            player2.HandSign = new HandSign(HandSign.Move.Rock);
+
+            string expectedMessage = "Congrats!! You are the winner";
+
+            string resultMessage = HandSign.GetWinner(player1, player2);
+            Assert.AreEqual(expectedMessage, resultMessage);
+        }
+
+        [Test()]
+        public void TestGetCPUWinner()
+        {
+            Player player1 = new Player("John");
+            Player player2 = new Player("CPU");
+
+            player1.HandSign = new HandSign(HandSign.Move.Paper);
+            player2.HandSign = new HandSign(HandSign.Move.Scissors);
+
+            string expectedMessage = "CPU is the winner";
+
+            string resultMessage = HandSign.GetWinner(player1, player2);
+            Assert.AreEqual(expectedMessage, resultMessage);
+        }
+
+        [Test()]
+        public void TestDraw()
+        {
+            Player player1 = new Player("John");
+            Player player2 = new Player("CPU");
+
+            player1.HandSign = new HandSign(HandSign.Move.Paper);
+            player2.HandSign = new HandSign(HandSign.Move.Paper);
+
+            string expectedMessage = "OOPS!! It's a Draw!";
+
+            string resultMessage = HandSign.GetWinner(player1, player2);
+            Assert.AreEqual(expectedMessage, resultMessage);
+        }
+
+        [Test()]
+        public void TestGetWinningMove()
+        {
+            HandSign.Move move1 = HandSign.Move.Rock;
+
+            string expectedWinningMove = HandSign.Move.Paper.ToString();
+
+            string resultWinningMove = HandSign.GetWinningMove(move1).ToString();
+            Assert.AreEqual(expectedWinningMove, resultWinningMove);
+        }
+    }
+}
---
+++ Test/PlayerTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+namespace RockPaperScissors.Test
+{
+    [TestFixture]
+    public class PlayerTest
+    {
+        [Test]
+        public void TestPlayerNameCase()
+        {
+            string expected = "John";
+            Player player = new Player("John");
+            string playerName = player.Name;
+            Assert.AreEqual(expected, playerName);
+        }
+    }
+}
